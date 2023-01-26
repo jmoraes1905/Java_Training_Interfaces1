ba6436bcf4fc6a96e4e8e11d99d2cf5e9c3c5830
Joel  Godoy <j.godoy.moraes@gmail.com>
2023-01-26 11:25:20 -0300

    Solution without Interfaces

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 
 import Model.entities.CarRental;
 import Model.entities.Vehicle;
+import Model.services.BrazilTaxService;
+import Model.services.RentalService;
 
 public class Program {
 
@@ -14,18 +16,31 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		
 		System.out.println("Enter with rent data:");
 		System.out.println("Car model:");
 		String carModel = sc.nextLine();
-		System.out.println("Rent date (dd/MM/yyyy)");
+		System.out.println("Rent date (dd/MM/yyyy hh:mm)");
 		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
-		System.out.println("Return date (dd/MM/yyyy)");
+		System.out.println("Return date (dd/MM/yyyy hh:mm)");
 		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(),fmt);
 		
 		CarRental cr = new CarRental(start,finish,new Vehicle(carModel));
 		
+		System.out.println("Enter the price per hour:");
+		double pricePerHour = sc.nextDouble();
+		System.out.println("Enter price per day:");
+		double pricePerDay = sc.nextDouble();
+		
+		RentalService rentalService = new RentalService(pricePerHour,pricePerDay,new BrazilTaxService());
+		
+		rentalService.processInvoice(cr);
+		
+		System.out.println("FATURA:");
+		System.out.println("Basic Payment: "+ cr.getInvoice().getBasicPayment());
+		System.out.println("Tax: " + cr.getInvoice().getTax());
+		System.out.println("Total payment: " + cr.getInvoice().getTotalPayment());
 		sc.close();
 	}
 
--- src/Model/entities/Invoice.java
+++ src/Model/entities/Invoice.java
@@ -32,6 +32,6 @@ public class Invoice {
 	}
 	
 	public Double getTotalPayment() {
-		return getBasicPayment()-getTax();
+		return getBasicPayment()+getTax();
 	}
 }
